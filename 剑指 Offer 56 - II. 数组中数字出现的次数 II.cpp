678d3303fd760e787c1284f16484969023e1ecd9
watermelonyip <75349382+watermelonyip@users.noreply.github.com>
2021-03-07 16:05:40 +0800

    Update 剑指 Offer 56 - II. 数组中数字出现的次数 II.cpp

--- 剑指 Offer 56 - II. 数组中数字出现的次数 II.cpp
+++ 剑指 Offer 56 - II. 数组中数字出现的次数 II.cpp
@@ -4,6 +4,8 @@
 思路：数组里每个数组的对应位相加起来，最后得到的结果如果该位能整除3，表示只出现一次的数字该位位0；如果不能整除3，表示只出现一次的数字该位为1。
 时间复杂度O(n)，空间复杂度O(1)，其中n为数组大小。
 还可以用位运算的方法，参考：https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-ii-lcof/solution/mian-shi-ti-56-ii-shu-zu-zhong-shu-zi-chu-xian-d-4/
+
+要注意一些自己容易错的细节，bit的类型是unsigned int，不然会溢出；(nums[i] & bit) != 0 这里的判断条件不要写成==1。
 */
 class Solution {
 public:
